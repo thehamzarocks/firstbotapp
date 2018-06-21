f9a2eb8c5dd7accafb3fe14c379d7b009607ff8f
thehamzarocks <thehamzarocks@gmail.com>
2018-06-22 01:56:35 +0530

    ui ready

--- src/app/chats/chat/chat.component.ts
+++ src/app/chats/chat/chat.component.ts
@@ -13,3 +13,17 @@ export class ChatComponent implements OnInit {
   }
 
 }
+
+// Function to replace one element with another
+// var str = '<a href="http://www.com">item to replace</a>'; //it can be anything
+// var Obj = document.getElementById('TargetObject'); //any element to be fully replaced
+// if(Obj.outerHTML) { //if outerHTML is supported
+//     Obj.outerHTML=str; ///it's simple replacement of whole element with contents of str var
+// }
+// else { //if outerHTML is not supported, there is a weird but crossbrowsered trick
+//     var tmpObj=document.createElement("div");
+//     tmpObj.innerHTML='<!--THIS DATA SHOULD BE REPLACED-->';
+//     ObjParent=Obj.parentNode; //Okey, element should be parented
+//     ObjParent.replaceChild(tmpObj,Obj); //here we placing our temporary data instead of our target, so we can find it then and replace it into whatever we want to replace to
+//     ObjParent.innerHTML=ObjParent.innerHTML.replace('<div><!--THIS DATA SHOULD BE REPLACED--></div>',str);
+// }
