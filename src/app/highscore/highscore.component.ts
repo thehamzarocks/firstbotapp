ee088027bef10e811e11017077d06e2aa99197c0
thehamzarocks <thehamzarocks@gmail.com>
2018-07-21 21:31:52 +0530

    updating high scores component

--- src/app/highscore/highscore.component.ts
+++ src/app/highscore/highscore.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { IPlayer } from './playerobject';
+import { AngularFirestore } from 'angularfire2/firestore';
 
 @Component({
   selector: 'app-highscore',
@@ -7,9 +9,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HighscoreComponent implements OnInit {
 
-  constructor() { }
+  players: IPlayer[];
+
+  constructor(private db:AngularFirestore) { }
 
   ngOnInit() {
+    this.players = new Array<IPlayer>();
+
+    this.db.collection("players").ref.get().then((querySnapshot) => {
+      querySnapshot.forEach((doc) => {
+        this.players.push({
+          playerName: doc.data().name,
+          points: doc.data().points,
+        });        
+      });
+      this.players.sort((player1,player2) => {
+        return -(player1.points - player2.points);
+      })
+      });
   }
 
 }
